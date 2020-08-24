44bfd1513998ac6f79fc90e28cd22daf261cb600
luisnasc <luisbrunu@gmail.com>
2020-08-24 03:54:54 -0300

    start rename

---
+++ customer/src/test/java/com/livecoding/dio/custumer/costumer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livecoding.dio.custumer.costumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
