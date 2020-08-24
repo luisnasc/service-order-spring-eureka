8de23ff6883d7836b5249766c36ab39a254805b5
luisnasc <luisbrunu@gmail.com>
2020-08-24 05:14:10 -0300

    Remove duplicated directory

--- costumer/src/main/java/com/livecoding/dio/custumer/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.livecoding.dio.custumer.repository;
-
-import com.livecoding.dio.custumer.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
